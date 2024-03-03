2d1309fb548874d4d8d31aa177b13aa7413075c5
Lucas <lucasoliveirars22@gmail.com>
2024-03-02 23:07:37 -0300

    add status_display

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -2,9 +2,13 @@ from rest_framework import serializers
 
 from tasks.models import Task
 
+
 class TaskSerializer(serializers.ModelSerializer):
-    status = serializers.CharField(source="get_status_display")
-    
+    status_display = serializers.SerializerMethodField()
+
     class Meta:
         model = Task
-        fields = ["id", "title", "status", "description"]
+        fields = ["id", "title", "status", "description", "status_display"]
+
+    def get_status_display(self, obj):
+        return obj.get_status_display()
